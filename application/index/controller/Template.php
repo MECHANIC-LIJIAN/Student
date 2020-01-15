8797a8a58bdef00f5d79385f9250d3a8a6dc0e8c
lijian <lijianwzx@163.com>
2020-01-15 14:30:57 +0800

    1.0

--- application/index/controller/Template.php
+++ application/index/controller/Template.php
@@ -11,11 +11,11 @@ class Template extends Controller
     public function readTemplate()
     {
         $id = input('id');
-        $startTime = time(); //返回当前时间的Unix 时间戳
+        
         $template = model("Templates")->with('getoption')->where(['tid' => $id])->find()->toArray();
-        // dump($template['getoption']);
+        
         $optionList = getOptionList($template['getoption'], $pid = 'pid', $id = 'sid');
-        // dump($optionList);
+        
         return $this->fetch('index', ['optionList' => $optionList, 'tname' => $template['tname']]);
     }
 }
