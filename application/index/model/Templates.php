1fe706b5ba0c054bf515b35a86d818102bc83a69
lijian <lijianwzx@163.com>
2020-01-16 15:09:24 +0800

    1.0

---
+++ application/index/model/Templates.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\index\model;
+
+use think\Model;
+use think\model\concern\SoftDelete;
+
+class Templates extends Model
+{
+    use SoftDelete;
+
+    public function options()
+    {
+        return $this->hasMany('TemplatesOption', 'tid', 'tid')->field('tid,sid,pid,title');
+    }
+
+    
+}
