07c2b90f1d97c930ce8f43cb6e5a866012f9e741
lijian <lijianwzx@163.com>
2020-04-26 15:56:51 +0800

    2.0

--- route/cov.php
+++ route/cov.php
@@ -7,7 +7,7 @@ Route::group(
     'admin/cov',
     function () {
         Route::get('index', 'admin/cov/index');
-        Route::post('newreport', 'admin/cov/newreport');
+        Route::post('newreport', 'admin/cov/newReport');
         Route::rule('per_day_reports/:date', 'admin/cov/perDayReports','GET|POST');
         Route::rule('down_per_day_reports', 'admin/cov/downPerDayReports','GET|POST');
 
