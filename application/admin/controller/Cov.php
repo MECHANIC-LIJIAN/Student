88c81c77a3e7f011e333a62a503fad5046e381ff
lijian <lijianwzx@163.com>
2020-05-13 22:45:47 +0800

    2.0

--- application/admin/controller/Cov.php
+++ application/admin/controller/Cov.php
@@ -194,7 +194,7 @@ class Cov extends Base
             }
         }
         $instructorIds = Db::name("auth_group_access")->where(['group_id' => 9])->column('uid');
-        $pids=Db::name('cov_users')->table('stu_cov_users')->where('uid', $this->uid)->column('pid');
+        $pids=Db::name('cov_users')->where('uid', $this->uid)->column('pid');
         $instructorId=array_intersect($pids,$instructorIds);
         dump($instructorIds);
         dump($pids);
