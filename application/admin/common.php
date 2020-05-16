cf0181ed36879d2954613dee79f9efd241be3894
lijian <lijianwzx@163.com>
2020-05-16 20:31:44 +0800

    2.0

--- application/admin/common.php
+++ application/admin/common.php
@@ -111,3 +111,39 @@ function delByValue($arr, $value){
     }
     return $arr;
   }
+
+
+
+   /**
+     * 直接输出二维码 + 生成二维码图片文件
+     */
+    function create(){
+        // 自定义二维码配置
+        $config = [
+            'title'         => true,
+            'title_content' => '嗨，老范',
+            'logo'          => true,
+            'logo_url'      => './logo.png',
+            'logo_size'     => 80,
+        ];
+
+        // 直接输出
+        $qr_url = 'http://www.baidu.com?id=' . rand(1000, 9999);
+
+        $qr_code = new QrcodeServer($config);
+        $qr_img = $qr_code->createServer($qr_url);
+        echo $qr_img;
+
+        // 写入文件
+        $qr_url = '这是个测试二维码';
+        $file_name = './static/qrcode';  // 定义保存目录
+
+        $config['file_name'] = $file_name;
+        $config['generate']  = 'writefile';
+
+        $qr_code = new QrcodeServer($config);
+        $rs = $qr_code->createServer($qr_url);
+        print_r($rs);
+
+        exit;
+    }
