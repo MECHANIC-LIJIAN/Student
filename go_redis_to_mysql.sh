fb8bfdbec5b67fb8d923255b741bda8bc69e41f5
lijian <lijianwzx@163.com>
2020-09-13 17:13:25 +0800

    增加手机号规则

--- go_redis_to_mysql.sh
+++ go_redis_to_mysql.sh
@@ -6,7 +6,7 @@ echo "the insert process is runing at $PIDS!"
 else
 ./redis_to_mysql.sh
 PID=`ps -ef|grep 'index.php datasToMysql'|grep -v grep|awk '{print $2}'`
-echo "the process has success runing at $PID!"
+echo "the insert process has success runing at $PID!"
 #运行进程
 fi
 
