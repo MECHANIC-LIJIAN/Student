7c368cdc5284d3a336b1726434cc16aded068fc8
lijian <lijianwzx@163.com>
2020-09-14 19:54:34 +0800

    未填写数据查询

--- application/admin/controller/Template.php
+++ application/admin/controller/Template.php
@@ -102,24 +102,25 @@ class Template extends Base
             ->with('getMyData')
             ->find();
         
-        // $noList = Db::name('MyDataOption')
-        //     ->where('my_data_id', $template['get_my_data']['id'])
-        //     ->where('content', 'NOTIN', function ($query) use ($tId, $template) {
-        //         $query->table('stu_templates_datas')
-        //             ->where('tid', $tId)
-        //             ->field("content->" . $template['primaryKey']);
-        //     })
-        //     ->column('content');
+        $noList = Db::name('MyDataOption')
+            ->where('my_data_id', $template['get_my_data']['id'])
+            ->where('content', 'NOTIN', function ($query) use ($tId, $template) {
+                $query->table('stu_templates_datas')
+                    ->where('tid', $tId)
+                    ->field("content->" . $template['primaryKey']);
+            })
+            
         // ->fetchSql()
+            ->column('content');
         
-        $myDataList=Db::name('MyDataOption')
-        ->where('my_data_id', $template['get_my_data']['id'])
-        ->column('content');
-        $hasList=Db::name('templates_datas')
-        ->where('tid', $tId)
-        ->field("content->" . $template['primaryKey'])
-        ->select();
-        $noList=array_diff($myDataList,$hasList);
+        // $myDataList=Db::name('MyDataOption')
+        // ->where('my_data_id', $template['get_my_data']['id'])
+        // ->column('content');
+        // $hasList=Db::name('templates_datas')
+        // ->where('tid', $tId)
+        // ->field("content->" . $template['primaryKey'])
+        // ->select();
+
         if (!empty($noList)) {
             $this->success("请求未填写数据成功", "", $noList);
         } else {
